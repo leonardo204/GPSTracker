aae6d8339e295d3bb336bd5f91da81ea91eb6626
leonardo204 <zerolive7@gmail.com>
2017-10-06 16:31:31 +0900

    Add google plist

---
+++ GPSTracker/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GPSTracker
+//
+//  Created by zerolive on 2017. 10. 6..
+//  Copyright © 2017년 Leonardo204. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mailTextForm: UITextField!
+    @IBOutlet weak var passwdTextForm: UITextField!
+    @IBOutlet weak var regButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func processRegButton(_ sender: Any) {
+    }
+    @IBAction func processLoginButton(_ sender: Any) {
+    }
+    
+}
+
